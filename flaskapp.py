2a78073969a2d68f56790874c9fdd0f1b3261b18
BPayne22 <104167769+BPayne22@users.noreply.github.com>
2025-07-10 16:11:05 -0700

    Update flaskapp.py
    Deleted trailing path to fix cors issue

--- flaskapp.py
+++ flaskapp.py
@@ -8,7 +8,7 @@ CORS(app, resources={r"/*": {"origins": [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://rylandbangerter.github.io",
-    "https://686e981c9930ce00086f44c9--merry-gnome-9ee3d2.netlify.app/mbgpt/"
+    "https://686e981c9930ce00086f44c9--merry-gnome-9ee3d2.netlify.app"
 ]}}, supports_credentials=True)
 
 
