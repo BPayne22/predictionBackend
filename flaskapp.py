f3ba9534485024af02fe9a5548be7de4f211a187
BPayne22 <104167769+BPayne22@users.noreply.github.com>
2025-07-14 11:18:39 -0600

    Update flaskapp.py
    cors

--- flaskapp.py
+++ flaskapp.py
@@ -11,6 +11,7 @@ CORS(app, origins=[
     "http://localhost:5500",
     "https://rylandbangerter.github.io",
     "https://686e981c9930ce00086f44c9--merry-gnome-9ee3d2.netlify.app"
+    "https://686e981c9930ce00086f44c9--merry-gnome-9ee3d2.netlify.app/mbgpt/"
 ], supports_credentials=True)
 
 @app.route("/predict", methods=["POST", "OPTIONS"])
